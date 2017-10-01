5e3010127822e2144357395c174253d274d0b324
deepak <navneet.prakash@manprax.com>
2017-10-01 23:28:40 +0530

    basic admin interface added

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+# from nested_inline.admin import NestedStackedInline, NestedModelAdmin
+from core.models import *
 
 # Register your models here.
+
+# class NotesInline(NestedStackedInline):
+#     """
+#     Admin view for adding and viewing notes.
+#     """
+#     model = Notes
+#     extra = 1
+#     fk_name = 'category'
+
+# class QuestionPaperInline(NestedStackedInline):
+#     """
+#     Admin view for adding and viewing question papers.
+#     """
+#     model = QuestionPaper
+#     extra = 1
+#     fk_name = 'category'
+
+# class NotesAdmin(NestedModelAdmin):
+#     inlines = [
+#         NotesInline
+#     ]
+
+# class QuestionPaperAdmin(NestedModelAdmin):
+#     inlines = [
+#         QuestionPaperInline
+#    ]
+admin.site.register(Category)
+admin.site.register(Notes)
+admin.site.register(QuestionPaper)
+# admin.site.register(NotesAdmin)
+# admin.site.register(QuestionPaperAdmin)
